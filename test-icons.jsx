f61a42e109d3171104f6836507be57c2f9081018
Satyam Ashtikar <sattyashtikar@gmail.com>
2026-06-09 18:35:42 +0530

    Updates

--- test-icons.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { SiPython, SiCplusplus, SiTypescript, SiPytorch, SiScikitlearn, SiDocker, SiGooglecloud, SiGit, SiFastapi, SiNginx, SiMongodb, SiNeo4j, SiOpencv, SiLangchain } from "react-icons/si";
-
-console.log("Success");
--- test-icons2.jsx
+++
@@ -1,5 +0,0 @@
-import { renderToString } from 'react-dom/server';
-import { GiFirecracker } from "react-icons/gi";
-import { TbLetterM } from "react-icons/tb";
-
-console.log("Imports ok");
